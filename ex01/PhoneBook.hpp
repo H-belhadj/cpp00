35949cb9e630fbdfa42af60f8f2a9d71198c88c4
belhadj haitam <steankir.haitam@gmail.com>
2023-12-17 21:39:45 +0100

    start ex01

---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 21:30:41 by hbelhadj          #+#    #+#             */
+/*   Updated: 2023/12/17 21:39:17 by hbelhadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+#   define PHONEBOOK_H
+
+#include <iostream>
+#include "Contact.hpp"
+
+class PhoneBook
+{
+    private:
+        Contact  a[8];
+};
+
+#endif
