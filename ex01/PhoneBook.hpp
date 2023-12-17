9d81e53e2e5a2b83b2d377072526ae0f3ed5687a
belhadj haitam <steankir.haitam@gmail.com>
2023-12-17 22:14:50 +0100

    pf

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 21:30:41 by hbelhadj          #+#    #+#             */
-/*   Updated: 2023/12/17 21:39:17 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2023/12/17 22:06:16 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@
 class PhoneBook
 {
     private:
-        Contact  a[8];
+        Contact  contact[8];
+        static const size_t maxContacts = 8;
+    
 };
 
 #endif
